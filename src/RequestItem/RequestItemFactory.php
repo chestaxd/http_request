494aeffa719efbf578676ef91a2052c7b8e56c7f
chestaxd <chesta88@gmail.com>
2022-05-20 20:34:02 +0300

    added exceptions for wrong request data

--- src/RequestItem/RequestItemFactory.php
+++ src/RequestItem/RequestItemFactory.php
@@ -4,13 +4,17 @@ namespace App\RequestItem;
 
 class RequestItemFactory
 {
+    /**
+     * @throws \Exception
+     */
     public static function getRequestItem(array $requestData): RequestItem
     {
+        if (!isset($requestData['method'])) throw new \Exception('Missing request method');
         return match ($requestData['method']) {
             'GET' => GetRequestItem::fromArray($requestData),
             'POST' => PostRequestItem::fromArray($requestData),
+            default => throw new \Exception('Request method is not found')
         };
 
     }
-
 }
